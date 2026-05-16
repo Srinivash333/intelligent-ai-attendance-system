331f631bf4ab82d1ce73375c41ee5fc07a656305
Srinivas H <srinivashulihyderseenu@gmail.com>
2026-05-16 06:49:39 +0530

    home_screen

--- app.py
+++ app.py
@@ -1,45 +1,23 @@
-import streamlit as st
+import streamlit as st 
+
+
+from src.screens.home_screen import home_screen
+from src.screens.teacher_screen import teacher_screen
+from src.screens.student_screen import  student_screen
 
 def main():
+    if "login_type" not in st.session_state:
+        st.session_state["login_type"]=None
 
-    st.set_page_config(
-        page_title="AI Attendance System",
-        page_icon="📸",
-        layout="centered"
-    )
 
-    # Title
-    st.title("AI Powered Attendance System")
+    match st.session_state["login_type"]:
+        case "teacher":
+            teacher_screen()
 
-    # User input
-    name = st.text_input("Enter Student Name")
+        case "student":
+            student_screen()
 
-    # Buttons
-    col1, col2 = st.columns(2)
+        case None:
+            home_screen()    
 
-    with col1:
-        if st.button("Mark Attendance", type="primary", use_container_width=True):
-            st.success(f"Attendance Marked for {name}")
-
-    with col2:
-        if st.button("Exit", type="secondary", use_container_width=True):
-            st.warning("Session Closed")
-
-    # AI Attendance Image using HTML
-    st.markdown("""
-    <div style="text-align:center; margin-top:30px;">
-        <img 
-            src="https://images.unsplash.com/photo-1522075469751-3a6694fb2f61?q=80&w=1200&auto=format&fit=crop"
-            width="700"
-            style="border-radius:15px;"
-        >
-        <h2 style="color:#4CAF50;">
-            Face Recognition Attendance System
-        </h2>
-        <p>
-            AI based smart attendance using facial recognition technology.
-        </p>
-    </div>
-    """, unsafe_allow_html=True)
-
-main()
+main()            
